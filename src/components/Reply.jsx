2c2b1b976a3ca7fa72d6f068ba63cdb7ca4a2690
norristown <jarvasity@gmail.com>
2024-03-31 16:37:27 -0400

    refactored code. everything seems to be working here.

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -3,20 +3,18 @@ import { useSelector } from 'react-redux';
 import Timestamp from './Timestamp';
 import ModalReply from './ModalReply';
 
-const Reply = ({ replyId, handleReply }) => {
+const Reply = ({ replyId }) => {
   const comments = useSelector((state) => state.thread.comments);
   const nestedComments = comments.filter((c) => c.parentId === replyId);
 
   if (nestedComments.length > 0)
     return nestedComments.map((c) => (
-      <div className="mx-4" key={c.id}>
+      <div className="mx-2" key={c.id}>
         <Timestamp c={c} />
         <div className="flex justify-between">
           <span>{c.text}</span>
-          {/* <button onClick={() => handleReply(c.id)}>REPLY</button> */}
           <ModalReply replyId={c.id} />
         </div>
-        {/* <Reply handleReply={handleReply} replyId={c.id} /> */}
         <Reply replyId={c.id} />
       </div>
     ));
--- src/components/Thread.jsx
+++ src/components/Thread.jsx
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { addReply, getComments, getThreads } from '../reducers/threadReducer';
-import { useState } from 'react';
+import { getComments, getThreads } from '../reducers/threadReducer';
+
 import Reply from './Reply';
 import Timestamp from './Timestamp';
 import ModalComment from './ModalComment';
@@ -23,24 +23,6 @@ const Thread = () => {
 
   const thread = threads.find((t) => t.id === id);
 
-  const [reply, setReply] = useState('');
-  const [visible, setVisible] = useState(false);
-  const [replyId, setReplyId] = useState('');
-
-  const handleReply = () => {
-    setVisible(!visible);
-    dispatch(addReply(replyId, { comment: reply }));
-    setReply('');
-  };
-
-  const getId = (id) => {
-    setReplyId(id);
-    setVisible(!visible);
-    console.log(id);
-  };
-
-  const hide = { display: !visible ? 'none' : '' };
-
   return threads.length === 0 ? (
     <div>loading...</div>
   ) : (
@@ -51,10 +33,6 @@ const Thread = () => {
         </h1>
         <div className="mb-2 ml-4 p-4 bg-zinc-400 text-zinc-900">{thread.content}</div>
         <ModalComment />
-        <div style={hide}>
-          <input className="text-black" value={reply} onChange={(e) => setReply(e.target.value)} />
-          <button onClick={() => handleReply()}>SUBMIT</button>
-        </div>
         {thread.comments.map((c, i) => (
           <div key={i} className="flex flex-col text-sm">
             <Timestamp c={c} />
@@ -65,13 +43,12 @@ const Thread = () => {
             {comments.map(
               (reply) =>
                 reply.parentId === c.id && (
-                  <div className="flex flex-col ml-4" key={reply.id}>
+                  <div className="flex flex-col mx-2" key={reply.id}>
                     <Timestamp c={c} />
                     <div className="flex justify-between">
                       <span>{reply.text}</span>
                       <ModalReply replyId={reply.id} />
                     </div>
-                    {/* <Reply handleReply={getId} replyId={reply.id} /> */}
                     <Reply replyId={reply.id} />
                   </div>
                 )
