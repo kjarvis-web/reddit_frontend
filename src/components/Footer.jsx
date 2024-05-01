7be41464b677006a477d16392d1b434e85b4dc3a
norristown <jarvasity@gmail.com>
2024-04-30 20:30:12 -0400

    fixed github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className="flex justify-center items-center p-2 bg-zinc-800 text-zinc-100 mt-8">
-      <Link className="flex items-center gap-2 group" to="https://github.com/norristown">
+      <Link className="flex items-center gap-2 group" to="https://github.com/kjarvis-web">
         <p>Kevin Jarvis</p>
         <VscGithub className="h-10 w-10 group-hover:text-orange-600" />
       </Link>
